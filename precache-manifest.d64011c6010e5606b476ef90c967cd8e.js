b29e44edb152a596e0a21c78f4bcd945bc6e521a
siddharth009 <spaudel206@gmail.com>
2020-05-13 14:16:29 +0545

    Updates

---
+++ precache-manifest.d64011c6010e5606b476ef90c967cd8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3423948f6fb12b7aaba55205bf73a96a",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/index.html"
+  },
+  {
+    "revision": "1497f65cc4b75c83c9e5",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/static/css/2.b8b3bc4e.chunk.css"
+  },
+  {
+    "revision": "5812d8642e377a32864d",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/static/css/main.55c1043d.chunk.css"
+  },
+  {
+    "revision": "1497f65cc4b75c83c9e5",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/static/js/2.3c8bb370.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/static/js/2.3c8bb370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5812d8642e377a32864d",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/static/js/main.cbfbff07.chunk.js"
+  },
+  {
+    "revision": "4b4cdfc38ee27ad1e536",
+    "url": "/git@github.com:Siddharth009/hawa-react.git/static/js/runtime-main.32bf5fda.js"
+  }
+]);
